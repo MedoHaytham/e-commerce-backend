0d311f607ae4c88878584f61037712cd8d149ed7
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-03-24 00:38:08 +0200

    feat: add Zod schema for product validation

--- schemas/productsSchema.js
+++ schemas/productsSchema.js
@@ -5,7 +5,6 @@ export const productsSchema = z.object({
   description: z.string().min(1, 'description is required').max(800, 'description must be at most 800 characters long'),
   category: z.string().min(1, 'category is required'),
   price: z.number().min(1, 'price is required'),
-  rating: z.number().min(0).max(5),
   stock: z.number().min(0, 'stock must be at least 0'),
   brand: z.string().min(1, 'brand is required'),
   availabilityStatus: z.enum(["In Stock", "Out of Stock"]).default("In Stock"),
